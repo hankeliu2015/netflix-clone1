67d80e53f9bfa9599d324d3ca04084d7db062adb
Hank Liu <hanke.liu@gmail.com>
2020-05-27 16:56:46 -0400

    js script added for tab content

--- javascript/main.js
+++ javascript/main.js
@@ -4,8 +4,12 @@ const tabContentItems = document.querySelectorAll(".tab-content-item")
 
 function selectItem(e) {
   removeBorder();
+  removeShow();
+  // let currentId = parseInt(e.currentTarget.id.slice(-1)) - 1;
+  let currentId = parseInt(this.id.slice(-1)) - 1;
+
   this.classList.add('tab-border');
-  console.log("clicked")
+  tabContentItems[currentId].classList.add("show")
 }
 
 // remove all bottom border from all tab item
@@ -14,6 +18,15 @@ function removeBorder() {
     item.classList.remove('tab-border')
   })
 }
+
+// remove show class from all tab contents
+function removeShow() {
+  tabContentItems.forEach(item => {
+    item.classList.remove('show')
+  })
+}
+
+
 //listen tab click function
 
 tabItems.forEach(item => item.addEventListener('click', selectItem))
